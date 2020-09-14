2dd5e56eca01d20e07c3f6fe2e2da532658c4024
obrunofontana <53912936+obrunofontana@users.noreply.github.com>
2020-09-14 17:38:00 -0300

    Update exercise_six.py
    Corrige algoritmo

--- exercise_six.py
+++ exercise_six.py
@@ -2,10 +2,12 @@ number_max_sequences = int(input('Quantas sequências Fibonacci deseja visualiza
 
 previous = 0
 next = 0
+count = 0
 
-while(next < number_max_sequences):
+while(count < number_max_sequences):
     print(next)
     next = next + previous
     previous = next - previous
     if next == 0:
         next = next + 1
+    count += 1
